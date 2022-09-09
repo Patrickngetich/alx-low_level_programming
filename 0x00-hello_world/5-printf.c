a07a42d2d5e0993f844e17aacae5f015b9055e8e
root <you@exampl.com>
2022-09-09 12:22:43 -0700

    #i
    git commit -m #i

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,24 +1,23 @@
-#!/bin/bash
 #include <stdio.h>
 
 
+ /**
+ 
+  * main - main block
+ 
+  * Return: 0
+ 
+ 
+  */
 
-/**
- *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
 
 
-
-int main(void)
+    int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+        printf("with proper grammar, but the outcome is a piece of art,\n");
 
-			return (0);
+        return (0);
 
 }
