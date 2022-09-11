3c21f44e60605c2295fcf2251892aa1e8c50323d
root <you@exampl.com>
2022-09-11 14:10:39 -0700

    0-positive_or_negative.c

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -8,27 +8,27 @@
 
 /**
 
-* main - check if a random number is positive or negative
-
-* Return: 0 if error
-
-*/
+ *  * main - check if a random number is positive or negative
+ *
+ *   * Return: 0 if error
+ *
+ *    */
 
 int main(void)
 
 {
 
-	int n;
+		int n;
 
 
 
-	srand(time(0));
+			srand(time(0));
 
-	n = rand() - RAND_MAX / 2;
+				n = rand() - RAND_MAX / 2;
 
 
 
-        (n > 0)
+        if (n > 0)
 
 	printf("%d is positive\n", n);
 
